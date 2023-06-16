c90c6e617aa7efe7385e9abeb72adb77ac261f9c
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-06-16 06:21:07 -0700

    v

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <sdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
