82445ac6c5a2f84a3c638f319cfbf96671cd3648
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-03 07:54:07 -0700

    g

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string.
+ *
+ * @s: string
+ * @c: character
+ *
+ * return: s
+*/
+
+char *_strchr(char *s, char c)
+{
+	int lk;
+
+	for (lk = 0; s[lk] >= '\0' ; lk++)
+	{
+		if (s[lk] == c)
+		{
+			return (s + lk);
+		}
+	}
+
+	return ('\0');
+}
