ad5a1645fa3b430675f51ceae529e742923a31db
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-06 08:06:07 -0700

    g

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - a function that prints a string in reverse
+ *
+ * @s: pointer to stg
+ *
+ * Return: 0
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		print _print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
