dd3ed48725718a264b4c874f3c5314c7889d3cbe
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-07 07:04:57 -0700

    j

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers.
+ * @argc: nb
+ * @argv: pointer
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
