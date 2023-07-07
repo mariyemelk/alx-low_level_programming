16cf6d71c04885909f5060dfe94659fcf614876c
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-07 04:05:40 -0700

    g

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argv: list
+ * @argc: in
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
