233b3685a57202a2fd077e69aee8673b218ff553
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-07 08:09:50 -0700

    y

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int u, cen = 0, mn = atoi(argv[1]);
+		int u, leastcents = 0, mn = atoi(argv[1]);
 		int cents[] = {25, 10, 5, 2, 1};
 
 		for (i = 0; i < 5; i++)
 		{
 			if (mn >= cents[u])
 			{
-				cen += mn / cents[u];
+				leastcents += mn / cents[u];
 				mn = mn % cents[i];
 				if (money % cents[u] == 0)
 				{
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
-		printf("%d\n", cen);
+		printf("%d\n", leastcents);
 	}
 	else
 	{
