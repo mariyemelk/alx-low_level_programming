fd7f45483a019687aa18fc052c2141b0542bbd3d
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-07 04:14:08 -0700

    h

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.c>
+
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argc: nb cmm line
+ * @argv: ponter
+ * Return: 0 on success
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
