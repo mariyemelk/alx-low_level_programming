ead7b787d512061c67fd453a7527843c0c19b5a3
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-10 06:40:23 -0700

    l

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strdup - function that returns a pointer to a newly allocated space
+ * @str: string
+ * Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	int b = 0, size = 0;
+	char *d;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	| d = malloc(size * sizeof(*str) + 1);
+
+	if (d == 0)
+		return (NULL);
+
+	else
+	{
+		for (; b < size; b++)
+			d[b] = str[b];
+	}
+	return (d);
+}
