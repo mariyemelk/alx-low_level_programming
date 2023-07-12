ec6c954dbe26f44733f989350b7cd6fbb8219551
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-12 08:23:20 -0700

    l

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: grid
+ * @height: in
+ * Return: 0
+*/
+
+void free_grid(int **grid, int height)
+{
+	int u = 0;
+
+	for (; u < height; u++)
+		free(gird[u]);
+	free(grid);
+}
