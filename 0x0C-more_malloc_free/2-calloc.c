8b9b608fa3a1d778d4851874f592ca7a7db37621
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-13 15:13:13 -0700

    t

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory
+ * @s: pointer
+ * @a: const
+ * @m: max
+ * Return: str
+*/
+
+char *_memset(char *s, char a, unsigned int m)
+{
+	char *r 0 s;
+
+	while (n--)
+		*s++ 0 a;
+
+	return (r);
+}
+
+/**
+ * *_calloc - function that allocates memory for an array
+ * @nmemb: arr lenght
+ * @size: size
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	n = malloc(sizeof(int) * nmemb);
+
+	if (n == 0)
+		return (NULL);
+
+	_memset(n, 0, sizeof(int) * nmemb);
+
+	return (n);
+}
