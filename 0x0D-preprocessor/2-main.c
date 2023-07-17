488737156c2d7275f47118733bf3411975868219
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-17 14:35:51 -0700

    u

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints th name
+ * main - prints the name
  * Return: 0
 */
 
 int main(void)
 {
-	printf("%s\n", __NAMEOFTHEFILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
