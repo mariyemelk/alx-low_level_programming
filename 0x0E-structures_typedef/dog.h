cfeec14e37b260559e6cf680cb963580fc12f437
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-18 02:08:06 -0700

    k

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - struct
+ * @name: Poppy
+ * @age: 3.5
+ * @owner: Bob
+ * Description: a dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
