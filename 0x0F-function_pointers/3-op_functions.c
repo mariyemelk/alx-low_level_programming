3f283020c947ae11b4a4f8a6dc84ea8d5407d958
Louakouzi Mariyeme <louakouzimariyeme@gmail.com>
2023-07-19 14:58:16 -0700

    yt

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subs integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sub
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - muls  integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: mul
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divs integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: div
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mods integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: mod
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
